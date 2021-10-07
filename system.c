e0dce05b8b6a90058b66ef9a3bb3e2b905b61506
Moises-Paz <moisespaz678@gmail.com>
2021-10-07 18:43:37 -0400

    implementacao da funcao 'novosProcessos' q' que m'dlkijmd alijdl'.

--- system.c
+++ system.c
@@ -1,3 +1,7 @@
+//Turma: 02
+//Disciplina: Sistemas Operacionais
+//Professor: Albert Franca Josua Costa.
+//Nome: Moises Paz Melo dos Santos.  <---
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -76,8 +80,8 @@ int converte(char entrada[], int *px){
                     return strtol(aux2, NULL, 10); //quando esta no formato pX-PY;
                 }
                 else{
-                    *px = -2;
-                    return -2;
+                    *px = -1;
+                    return -1;
                 }
             }
             else{
@@ -88,7 +92,7 @@ int converte(char entrada[], int *px){
         else{
             if(aux[0] != '\0') *px = strtol(aux, NULL, 10);
             else *px = -1;
-            return -2;//quando é o formato pX;
+            return -2;//quando é o formato pX -2222;
         }
     }
     else{
@@ -97,24 +101,93 @@ int converte(char entrada[], int *px){
     }
 }
 
-int main(){
-    char nome[15];
-    int retorno, px;
-
-    /*gets(nome);
-    retorno = converte(nome, &px);
-    if(retorno == -1 && px ==-1){
-        printf("nao esta no formato correto deve ser no formato px\n");
+NO* inicio(){
+    NO *novo = NULL;
+    novo = (NO *) malloc(sizeof(NO));
+    if(novo){
+        novo->nome = 'P';
+        novo->valor = 1;
+        novo->son = NULL;
+        novo->brother = NULL;
     }
-    else{
-        if(retorno == -2){
-            
-            printf("Px = %d\n",px);
+    return novo;
+}
+
+NO* buscaPai(NO **root, int pai){
+    if(*root){
+        if((*root)->valor == pai){
+            return *root;
         }
         else{
-            printf("Formato invalido dever ser PX\n");
+            NO *aux = buscaPai(&(*root)->brother, pai);
+            if(aux){
+                return aux;
+            }else{
+                return buscaPai(&(*root)->son, pai);
+            }
         }
     }
-    */
+    else{
+        return NULL;
+    }
+}
+
+void novosProcessos(NO **root, char valor[]){
+    if(*root){
+        int px, py;
+        py = converte(valor, &px);
+        if(py == -1 && px == -1) puts("Formato de entrada INVALIDO! O formato deve seguir o seguinte padrao: 'PX-PY'");
+        else{
+            if(py == -2) puts("Formato de entrada INVALIDO! Por vafor, informe a entrada no formato 'PX-PY'");
+            else{
+                if(px != py){
+                    NO *pai = buscaPai(root, px);
+                    if(pai){
+                        NO *novo = inicio();
+                        if(novo){
+                            novo->valor = py;
+                            novo->brother = pai->son;
+                            pai->son = novo;
+                        }
+                        else puts("Por algum motivo nao foi possivel criar novo processo.");
+                    }
+                    else printf("Pai nao encontrado!\n");
+                }
+                else puts("O pai nao pode ser igual ao filho.");
+            }
+        }
+    }
+}
+
+int main(){
+    char nome[15];
+    NO *root = inicio();
+    while(1){
+        fflush(stdin);
+        printf("Infome no fomato px-py: ");
+        gets(nome);
+        novosProcessos(&root, nome);
+        mostra(root);
+        puts("");
+    }
+        
+        
+    /*
+    NO *root = inicio();
+    printf("Infome no fomato px-py: ");
+    gets(nome);
+    novosProcessos(&root, nome);
+    mostra(root);
+    puts("");
+    printf("Infome no fomato px-py: ");
+    gets(nome);
+     novosProcessos(&root, nome);
+    mostra(root);
+    puts("");
+    printf("Infome no fomato px-py: ");
+    gets(nome);
+     novosProcessos(&root, nome);
+    mostra(root);
+    puts("");*/
     return 0;
 }
